09279024830ad4a7edee46ab810b122aef99a366
MrunaliGabhane <mrunagabhane@gmail.com>
2023-09-01 18:57:45 +0530

    Thought part completed

---
+++ aesop/src/Components/Thought/Thought.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../Thought/Thought.module.css"
+export const Thought = () => {
+  return (
+    <div className={styles.main}>
+        <p className={styles.thought}>‘Life never becomes a habit to me. It's always a marvel.’</p>
+        <p className={styles.name}>Katherine Mansfield</p>
+    </div>
+  )
+}
