d94e79e6be64a58f6addd31b559c91df51651fe8
MrunaliGabhane <mrunagabhane@gmail.com>
2023-09-01 19:17:30 +0530

    Thoughts section background change

--- aesop/src/Components/PostPoo/PostPoo.jsx
+++ aesop/src/Components/PostPoo/PostPoo.jsx
@@ -5,7 +5,7 @@ export const PostPoo = () => {
   return (
     <div className={styles.maindiv}>
       <div className={styles.childdiv}>
-      <div className={styles.rightdiv}>
+        <div className={styles.rightdiv}>
           <img
             src="https://www.aesop.com/u1nb1km7t5q7/6m4yuIyUcyfXMyKT17ZYsr/9bc9ba78322632d97323d73f364113f0/Aesop_Home_2023_Post-Poo_Drops_Web_Homepage_Secondary_Mid_Desktop_2560x1540px.png"
             alt=""
@@ -15,14 +15,15 @@ export const PostPoo = () => {
         <div className={styles.leftdiv}>
           <h1>Post-Poo Drops has returned</h1>
           <p>
-          Here to make the malodorous melodious once again. Dispense this favoured formulation into the toilet bowl after flushing to effectively mask disagreeable odours.
+            Here to make the malodorous melodious once again. Dispense this
+            favoured formulation into the toilet bowl after flushing to
+            effectively mask disagreeable odours.
           </p>
           <button className={styles.button}>
             Discover Post-Poo Drops
             <FaArrowRight className={styles.arrowIcon} />
           </button>
         </div>
-       
       </div>
     </div>
   );
